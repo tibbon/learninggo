fff59ede9550e7434bf63636c54a193b0e994348
David Fisher <tibbon+github@gmail.com>
2013-06-02 19:36:03 -0400

    Hello world made and compiled

---
+++ helloworld.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt" // Importing formattted i/o library
+
+/* This is a block comment that can go multiple lines, just like C */
+func main() {
+  fmt.Printf("Hello World")
+}
